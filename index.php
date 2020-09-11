4ced3519e1657b34d931830cbd3d3be41a25ef28
patriciaputripradani <patriciapraddani@gmailcom>
2020-09-11 17:30:54 +0700

    Pembuatan file index,php

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+echo "Animal <php>";
+echo " <hr>";
+
+class sapi{
+	   public $nama,  $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$sapi =  new sapi;
+	$sapi->nama = "raju";
+	$sapi->jumlah_kaki = "4";
+	$sapi->bisa_terbang = "tidak";
+	$sapi->suara = "mooo";
+
+	echo "sapi  <br>";
+	echo "nama : $sapi->nama <br>";
+	echo "jumlah kaki : $sapi->jumlah_kaki <br>";
+	echo "bisa terbang : $sapi->bisa_terbang <br>";
+	echo "suara : $sapi->suara <br>";
+	echo "<hr>";
+
+class kambing{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$kambing = new kambing;
+	$kambing->nama = "steven";
+	$kambing->jumlah_kaki = "4";
+	$kambing->bisa_terbang = "tidak";
+	$kambing->suara = "embekkk";
+
+
+	echo "kambing  <br>";
+	echo "nama : $kambing->nama <br>";
+	echo "jumlah kaki : $kambing->jumlah_kaki <br>";
+	echo "bisa terbang : $kambing->bisa_terbang <br>";
+	echo "suara : $kambing->suara <br>";
+	echo "<hr>";
+
+class merpati{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$merpati = new merpati;
+	$merpati->nama = "qila";
+	$merpati->jumlah_kaki = "2";
+	$merpati->bisa_terbang = "ya";
+	$merpati->suara = "krukk krukk";
+
+	echo "merpati  <br>";
+	echo "nama : $merpati->nama <br>";
+	echo "jumlah kaki : $merpati->jumlah_kaki <br>";
+	echo "bisa terbang : $merpati->bisa_terbang <br>";
+	echo "suara : $merpati->suara <br>";
+	echo "<hr>";
+
+class babi{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$babi = new babi;
+	$babi->nama = "alex";
+	$babi->jumlah_kaki = "4";
+	$babi->bisa_terbang = "tidak";
+	$babi->suara = "ngokk ngokk";
+
+	echo "babi  <br>";
+	echo "nama : $babi->nama <br>";
+	echo "jumlah kaki : $babi->jumlah_kaki <br>";
+	echo "bisa terbang : $babi->bisa_terbang <br>";
+	echo "suara : $babi->suara <br>";
+	echo "<hr>";
+
+class bebek{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$bebek = new bebek;
+	$bebek->nama = "michele";
+	$bebek->jumlah_kaki = "2";
+	$bebek->bisa_terbang = "ya";
+	$bebek->suara = "kuekk kuekk";
+
+	echo "bebek <br>";
+	echo "nama : $bebek->nama <br>";
+	echo "jumlah kaki : $bebek->jumlah_kaki <br>";
+	echo "bisa terbang : $bebek->bisa_terbang <br>";
+	echo "suara : $bebek->suara <br>";
+	echo "<hr>";
+
